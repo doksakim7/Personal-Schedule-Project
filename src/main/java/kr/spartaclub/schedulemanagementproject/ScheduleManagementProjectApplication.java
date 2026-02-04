f92285371191029e6f037a5ea41e90fd8076dde0
jhsky3118 <doksakim7@naver.com>
2026-02-04 12:49:17 +0900

    feat: initial commit

---
+++ src/main/java/kr/spartaclub/schedulemanagementproject/ScheduleManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.schedulemanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementProjectApplication.class, args);
+    }
+
+}
